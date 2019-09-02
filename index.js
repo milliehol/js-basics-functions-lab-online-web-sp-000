7b8ac9bbd41e6838a49de0c8f03414097a2856b9
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 04:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,3 +53,27 @@ distance1 = 1584;
 return distance1;
 
 }
+
+function calculatesFarePrice(distance1){
+
+if (distance1 === 43)
+distance1 = 1320;
+
+if (distance1 === 48)
+distance1 = 1320;
+
+if (distance1 === 50)
+distance1 = 2640;
+
+if (distance1 === 60)
+distance1 = 2640;
+
+if (distance1 === 34)
+distance1 = 1584;
+
+if (distance1 === 28)
+distance1 = 1584;
+
+return distance1;
+
+}
