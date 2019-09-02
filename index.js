db7548a53b39071a9ceb6585b5c78cc8d91aeeda
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 03:53:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(distance, distance2, distance3){
+function distanceFromHqInBlocks(distance1, distance2, distance3){
 
-if (distance = 43)
-distance = 1;
+if (distance1 = 43)
+distance1 = 1;
+
+else if (distance1 = 8)
+distance1 = 1;
 
 if (distance2 = 50)
 distance2 = 8;
@@ -11,7 +14,7 @@ distance2 = 8;
 if (distance3 = 34)
 distance3 = 8;
 
-return distance, distance2, distance3;
+return distance1, distance2, distance3;
 
 }
 
@@ -20,12 +23,15 @@ function distanceFromHqInFeet(distance1, distance2, distance3){
 if (distance1 = 43)
 distance1 = 264;
 
+else if (distance1 = 2112)
+distance1 = 264;
+
 if (distance2 = 50)
 distance2 = 2112;
 
 if (distance3 = 34)
 distance3 = 2112;
 
-return distance, distance2, distance3;
+return distance1, distance2, distance3;
 
 }
