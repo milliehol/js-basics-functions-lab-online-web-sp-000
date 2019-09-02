7a271ae8ac6c37fc37af886c245e31f6ca9bba7a
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 04:07:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,20 +15,17 @@ return distance1;
 
 }
 
-function distanceFromHqInFeet(distance1, distance2, distance3){
+function distanceFromHqInFeet(distance1){
 
-if (distance1 = 43)
+if (distance1 === 43)
 distance1 = 264;
 
-else if (distance1 = 2112)
-distance1 = 264;
+if (distance1 === 50)
+distance1 = 2112;
 
-if (distance2 = 50)
-distance2 = 2112;
+if (distance1 === 34)
+distance1 = 2112;
 
-if (distance3 = 34)
-distance3 = 2112;
-
-return distance1, distance2, distance3;
+return distance1;
 
 }
