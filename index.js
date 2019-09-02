6ff8f7a4fb97f16744c749346132088f9033f5c5
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 03:48:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ distance3 = 8;
 
 return distance, distance2, distance3;
 
+}
 
+function distanceFromHqInBlocks(distance, distance2, distance3){
+
+if (distance = 43)
+distance = 264;
+
+if (distance2 = 50)
+distance2 = 2112;
+
+if (distance3 = 34)
+distance3 = 2112;
+
+return distance, distance2, distance3;
 
 }
