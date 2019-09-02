b9838d8983328166f5ea439763d391c9a8918ffd
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 03:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function distanceFromHqInBlocks(distance, distance2, distance3){
 if (distance = 43)
 distance = 1;
 
-if (distance2 = 1)
+if (distance2 = 50)
 distance2 = 8;
 
-if (distance3 = 1)
+if (distance3 = 34)
 distance3 = 8;
 
 return distance, distance2, distance3;
