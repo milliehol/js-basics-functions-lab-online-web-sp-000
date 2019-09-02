a99fad42fc6892a27bb21b81137910477cb5f7bf
Carla Holcomb <carla.programmer@gmail.com>
2019-09-02 04:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(distance1, distance2, distance3){
+function distanceFromHqInBlocks(distance1){
 
-//if (distance1 === 43)
-//distance1 = 100;
+if (distance1 = 43)
+distance1 = 1;
 
-//else if (distance1 === 8)
-//distance1 = 100;
+else if (distance1 = 50)
+distance1 = 8;
 
-if (distance2 = 50)
-distance2 = 8;
+else if (distance1 = 34)
+distance1 = 8;
 
-if (distance3 = 34)
-distance3 = 8;
-
-return distance1, distance2, distance3;
+return distance1;
 
 }
 
